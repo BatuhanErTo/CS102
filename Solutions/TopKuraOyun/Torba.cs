28a3a893c33acf414ca10789df525341a09eb3a7
Batuhan Erol <bataer57@gmail.com>
2022-04-24 16:23:51 +0300

    Handling | Bugs

--- Solutions/TopKuraOyun/Torba.cs
+++ Solutions/TopKuraOyun/Torba.cs
@@ -8,7 +8,7 @@ namespace TopKuraOyun
 {
     internal class Torba
     {
-        Top[] top = new Top[50];
+        public Top[] top = new Top[50];
 
         public void Olustur()
         {
@@ -16,7 +16,7 @@ namespace TopKuraOyun
             int i = 0;
             while (i < 50)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < 10; j++)
                 {
                     top[i] = new Top();
                     top[i].turu = degerler[j];
@@ -28,15 +28,12 @@ namespace TopKuraOyun
         public void Shuffle()
         {
             Random rand = new Random();
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 50; i++)
             { 
-                for (int j = 0; j < 5; j++)
-                {
-                    int x = rand.Next(0, 50);
-                    Top temp = top[i];
-                    top[i] = top[j];
-                    top[j] = temp;
-                }
+                int x = rand.Next(0, 50);
+                Top temp = top[i];
+                top[i] = top[x];
+                top[x] = temp;  
             }
         }
 
