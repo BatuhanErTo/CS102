260719cd2408beaf8bed29d2ea1029e309855172
Batuhan Erol <bataer57@gmail.com>
2022-04-24 14:33:24 +0300

    Finished | TopKuraOyun

--- Solutions/TopKuraOyun/Oyun.cs
+++ Solutions/TopKuraOyun/Oyun.cs
@@ -10,34 +10,52 @@ namespace TopKuraOyun
     {
         Torba torba = new Torba();
         Player player1;
-        Player player2; // ?????????
+        Player player2;
+        int[] valuesOfBalls;
         public void Start()
         {
             player1 = new Player();
             player2 = new Player();
+            valuesOfBalls = new int[5];
             torba.Olustur();
             torba.Shuffle();
         }
 
         public void Play()
         {
+            playerChoosing();
+
+            computerChoosing();
+
+            Compare();
+
+            Display();
+        }
+
+        public void playerChoosing()
+        {
             for (int i = 0; i < 5; i++)
             {
-                Console.Write("1. Player please enter your {0} guess : ", i + 1);
+                Console.Write("1. Player please enter your {0}. guess : ", i + 1);
                 player1.choice[i] = Convert.ToInt32(Console.ReadLine());
             }
             for (int i = 0; i < 5; i++)
             {
-                Console.Write("2. Player please enter your {0} guess : ", i + 1);
+                Console.Write("2. Player please enter your {0}. guess : ", i + 1);
                 player2.choice[i] = Convert.ToInt32(Console.ReadLine());
             }
+        }
 
-            int[] valuesOfBalls = new int[5];
+        public void computerChoosing()
+        {
             for (int i = 0; i < 5; i++)
             {
                 valuesOfBalls[i] = torba.Draw();
             }
+        }
 
+        public void Compare()
+        {
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
@@ -53,7 +71,27 @@ namespace TopKuraOyun
                     }
                 }
             }
+        }
 
+        public void Display()
+        {
+            Console.WriteLine("==============================");
+            Console.Write("Player1 choices  ==> ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(player1.choice[i]+" ");
+            }
+            Console.Write("\nPlayer2 choices  ==> ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(player2.choice[i]+" ");
+            }
+            Console.Write("\nComputer choices  ==> ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(valuesOfBalls[i]+" ");
+            }
+            Console.WriteLine("\n==========================");
             if (player1.score > player2.score)
             {
                 Console.WriteLine("Player1 win");
@@ -66,8 +104,7 @@ namespace TopKuraOyun
             {
                 Console.WriteLine("Drawn");
             }
-
-
         }
+
     }
 }
