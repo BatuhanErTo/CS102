cbde19b13c301daa75bb65b745c2238d88f8fb52
Batuhan Erol <bataer57@gmail.com>
2022-04-24 16:51:42 +0300

    Added | HataSorusu

---
+++ Solutions/HataSorusu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HataSorusu // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double s = Metot(5, 2);
+        }
+
+        public static double Metot(int a, double b)
+        {
+            int sonuc = 0;
+            for (int i = 0; i < a; i++)
+            {
+                sonuc += Convert.ToInt32(b);
+            }
+            return sonuc;
+        }
+    }
+}
