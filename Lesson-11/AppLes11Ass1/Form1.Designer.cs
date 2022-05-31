25d8720232413253088d4d5d5b8d1d576dc9f4bf
Batuhan Erol <bataer57@gmail.com>
2022-05-31 20:55:20 +0300

    Added | Assignment1

---
+++ Lesson-11/AppLes11Ass1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace AppLes11Ass1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Çizgi çiz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Elips çiz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 59);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Kare çiz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(208, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 227);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(208, 271);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(780, 300);
+            this.panel2.TabIndex = 4;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 583);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Lesson-11/AppLes11Ass1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppLes11Ass1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics cizimAlani = panel1.CreateGraphics();
+            Pen kalem = new Pen(Color.Blue,2.4f);
+            cizimAlani.DrawRectangle(kalem, 20, 20, 20, 20);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics cizimAlani = panel1.CreateGraphics();
+            Pen kalem = new Pen(Color.Pink, 5.9f);
+            cizimAlani.DrawLine(kalem, 20, 31,100,100);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics cizimAlani = panel1.CreateGraphics();
+            Pen kalem = new Pen(Color.CornflowerBlue,4);
+            cizimAlani.DrawEllipse(kalem, 50, 50, 80, 70);
+        }
+
+        int[] x  = new int[2];
+        int[] y = new int[2];
+        int index1 = 0;
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            x[index1] = e.X; y[index1] = e.Y;
+            index1++;
+            if (index1 == 2)
+            {
+                index1 = 0;
+                Graphics cizimAlani = panel2.CreateGraphics();
+                Pen pen = new Pen(Color.Pink, 2f);
+                cizimAlani.DrawLine(pen,x[0],y[0],x[1],y[1]);
+            }
+        }
+    }
+}
