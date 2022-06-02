8fefc51b5e64ac61eae7472a9308039ccb8a017c
Batuhan Erol <bataer57@gmail.com>
2022-06-02 05:19:29 +0300

    Added | AppLes11Ass2

---
+++ Lesson-11/AppLes11Ass2/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace AppLes11Ass2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Renk Kutusu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Çizgi Kalınlığı";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(34, 145);
+            this.numericUpDown1.MinimumSize = new System.Drawing.Size(1, 0);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(31, 244);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Daire";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(31, 267);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Kare";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(31, 290);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(57, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Üçgen";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(31, 313);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Çizgi";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(31, 336);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(88, 17);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Serbest Çizim";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Çap veya Kenar Uzunluğu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 444);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(250, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(634, 468);
+            this.panel1.TabIndex = 10;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 552);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Lesson-11/AppLes11Ass2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppLes11Ass2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Color color;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            DialogResult result = colorDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                color = colorDialog.Color;
+            }
+        }
+        int[] ucgenCounterX = new int[3];
+        int[] ucgenCounterY = new int[3];
+        int ucgenCounter = 0;
+        int[] lineX = new int[2];
+        int[] lineY = new int[2];
+        int lineCounter = 0;
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                Graphics cizimAlani = panel1.CreateGraphics();
+                Pen pen = new Pen(color,Convert.ToInt32(numericUpDown1.Value));
+                cizimAlani.DrawEllipse(pen,e.X,e.Y,Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox1.Text));
+            }
+            else if (radioButton2.Checked == true)
+            {
+                Graphics cizimAlani = panel1.CreateGraphics();
+                Pen pen = new Pen(color, Convert.ToInt32(numericUpDown1.Value));
+                cizimAlani.DrawRectangle(pen,e.X,e.Y,Convert.ToInt32(textBox1.Text),Convert.ToInt32(textBox1.Text));
+            }
+            else if (radioButton3.Checked == true)
+            {
+                ucgenCounterX[ucgenCounter] = e.X;
+                ucgenCounterY[ucgenCounter] = e.Y;
+                ucgenCounter++;
+                if (ucgenCounter == 3)
+                {
+                    ucgenCounter = 0;
+                    Graphics cizimAlani = panel1.CreateGraphics();
+                    Pen pen = new Pen(color, Convert.ToInt32(numericUpDown1.Value));
+                    cizimAlani.DrawLine(pen,ucgenCounterX[0],ucgenCounterY[0],ucgenCounterX[1],ucgenCounterY[1]);
+                    cizimAlani.DrawLine(pen, ucgenCounterX[1], ucgenCounterY[1], ucgenCounterX[2], ucgenCounterY[2]);
+                    cizimAlani.DrawLine(pen, ucgenCounterX[2], ucgenCounterY[2], ucgenCounterX[0], ucgenCounterY[0]);
+                }
+            }
+            else if (radioButton4.Checked == true)
+            {
+                lineX[lineCounter] = e.X;
+                lineY[lineCounter] = e.Y;
+                lineCounter++;
+                if (lineCounter == 2)
+                {
+                    lineCounter = 0;
+                    Graphics cizimAlani = panel1.CreateGraphics();
+                    Pen pen = new Pen(color, Convert.ToInt32(numericUpDown1.Value));
+                    cizimAlani.DrawLine(pen,lineX[0],lineY[0], lineX[1], lineY[1]);
+                }
+            }
+        }
+        int x, y;
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (radioButton5.Checked == true)
+            {
+                x = e.X;
+                y = e.Y;
+                Graphics cizimAlani = panel1.CreateGraphics();
+                Pen pen = new Pen(color,5);
+                cizimAlani.DrawEllipse(pen,x,y,3,3);
+            }
+        }
+    }
+}
